7d07d3db6a815361a5760990bcf189ffb53a4a40
Justin Walrath <walrathjaw@gmail.com>
2014-08-23 12:29:18 -0500

    Split into modules.

--- Platformer1.py
+++ Platformer1.py
@@ -19,6 +19,9 @@ http://programarcadegames.com/python_examples/sprite_sheets/
 """
  
 import pygame
+
+from modules.sprites.Player import Player
+from modules.sprites.Wall import Wall
  
 """
 Global constants
@@ -32,79 +35,6 @@ BLUE     = (   0,   0, 255)
 # Screen dimensions
 SCREEN_WIDTH  = 800
 SCREEN_HEIGHT = 600
- 
-# This class represents the bar at the bottom that the player controls
-class Player(pygame.sprite.Sprite):
-    """ This class represents the bar at the bottom that the player controls. """
- 
-    # Set speed vector
-    change_x = 0
-    change_y = 0
-    walls = None
- 
-    # Constructor function
-    def __init__(self, x, y):
-        # Call the parent's constructor
-        pygame.sprite.Sprite.__init__(self)
- 
-        # Set height, width
-        self.image = pygame.Surface([15, 15])
-        self.image.fill(WHITE)
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
- 
-    def changespeed(self, x, y):
-        """ Change the speed of the player. """
-        self.change_x += x
-        self.change_y += y
- 
-    def update(self):
-        """ Update the player position. """
-        # Move left/right
-        self.rect.x += self.change_x
- 
-        # Did this update cause us to hit a wall?
-        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
-        for block in block_hit_list:
-            # If we are moving right, set our right side to the left side of the item we hit
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            else:
-                # Otherwise if we are moving left, do the opposite.
-                self.rect.left = block.rect.right
- 
-        # Move up/down
-        self.rect.y += self.change_y
- 
-        # Check and see if we hit anything
-        block_hit_list = pygame.sprite.spritecollide(self, self.walls, False)
-        for block in block_hit_list:
- 
-            # Reset our position based on the top/bottom of the object.
-            if self.change_y > 0:
-                self.rect.bottom = block.rect.top
-            else:
-                self.rect.top = block.rect.bottom
- 
-class Wall(pygame.sprite.Sprite):
-    """ Wall the player can run into. """
-    def __init__(self, x, y, width, height):
-        """ Constructor for the wall that the player can run into. """
-        # Call the parent's constructor
-        pygame.sprite.Sprite.__init__(self)
- 
-        # Make a blue wall, of the size specified in the parameters
-        self.image = pygame.Surface([width, height])
-        self.image.fill(BLUE)
- 
-        # Make our top-left corner the passed-in location.
-        self.rect = self.image.get_rect()
-        self.rect.y = y
-        self.rect.x = x
- 
  
 # Call this function so the Pygame library can initialize itself
 pygame.init()
---
+++ modules/sprites/Wall.py
@@ -0,0 +1,19 @@
+import pygame
+
+BLUE     = (   0,   0, 255)
+
+class Wall(pygame.sprite.Sprite):
+    """ Wall the player can run into. """
+    def __init__(self, x, y, width, height):
+        """ Constructor for the wall that the player can run into. """
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+ 
+        # Make a blue wall, of the size specified in the parameters
+        self.image = pygame.Surface([width, height])
+        self.image.fill(BLUE)
+ 
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.y = y
+        self.rect.x = x
