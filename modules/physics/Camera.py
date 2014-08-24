d6d5bcbbbec0a99bf61e35ac373d3183adc27def
Justin Walrath <walrathjaw@gmail.com>
2014-08-23 22:24:21 -0500

    Made it all black again.

--- modules/physics/Camera.py
+++ modules/physics/Camera.py
@@ -3,7 +3,7 @@ from pygame import *
 
 class Camera(object):
     def __init__(self, camera_func, width, height):
-    	print "Camera created"
+    	#print "Camera created"
         self.camera_func = camera_func
         self.state = Rect(0, 0, width, height)
 
